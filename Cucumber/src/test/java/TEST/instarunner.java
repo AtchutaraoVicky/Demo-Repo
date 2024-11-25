0aba34a7417d8bef13af97c758f7c869e6bd095b
Atchuth_Automation <atchuthvicky@gmail.com>
2024-11-25 16:09:56 +0530

    Update instarunner.java

--- Cucumber/src/test/java/TEST/instarunner.java
+++ Cucumber/src/test/java/TEST/instarunner.java
@@ -9,17 +9,4 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(features = "C:\\Users\\vicky\\OneDrive\\Desktop\\AUTOMATION_SELENIUM\\Cucumber\\src\\test\\resources\\Features\\Instagram.feature",glue="Stepdef")
 public class instarunner {
 
-  public void TC(){
-    System.out.println("Insta runner class");
-
-}
-  public void TC2(){
-	    System.out.println("this is mathod two");
-  }
-   public void Tc() {
-	   System.out.println(" git hub  change the message ");
-   }
-   public void TC4() {
-	   System.out.println(" This is method four ");
-   }
 }
