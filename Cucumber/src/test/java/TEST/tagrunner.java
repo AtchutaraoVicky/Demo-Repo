2a6163f541b3905db1a12cbdff5fc9e58d519a83
Atchuth_Automation <atchuthvicky@gmail.com>
2024-11-25 15:46:19 +0530

    Update tagrunner.java

--- Cucumber/src/test/java/TEST/tagrunner.java
+++ Cucumber/src/test/java/TEST/tagrunner.java
@@ -9,5 +9,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "C:\\Users\\vicky\\OneDrive\\Desktop\\AUTOMATION_SELENIUM\\Cucumber\\src\\test\\resources\\Features\\tag.feature",glue="Stepdef",tags="@smoketest")
 public class tagrunner {
+   public void TC(){
    System.out.println("Tag runner class passed");
+}
 }
